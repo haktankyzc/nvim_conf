93f8b8a82a22d2fc96547aa809da7531af0db41a
haktankyzc <haktankyzc@gmail.com>
2025-03-29 12:36:12 +0300

    multi-cursor plugin added

---
+++ lua/plugins/multi-cursor.lua
@@ -0,0 +1,9 @@
+-- plugins.lua veya ilgili dosyaya ekleyin
+return {
+  {
+    "mg979/vim-visual-multi",
+    config = function()
+      -- Eklenti yapılandırmasını buraya ekleyebilirsiniz
+    end
+  }
+}
